dbd4f62311fbaf36f6eadbe6e82866c0df68baf0
keishifujiwara <keishi0309@gmail.com>
2017-02-14 11:41:53 +0000

    followings-followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
+  end
 
   private
 
